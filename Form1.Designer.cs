b56bb269617bac7b44229bda45383cafe5d2e683
Joel <142605851+Joel-kaderli@users.noreply.github.com>
2025-05-19 09:43:02 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Snake_Winforms_Spiel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.StartButten = new System.Windows.Forms.Button();
+            this.Feld = new System.Windows.Forms.TableLayoutPanel();
+            this.Kopfpanel = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.Feld.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(702, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Geschwindigkeit";
+            this.label1.Click += new System.EventHandler(this.Geschwindigkeit_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(688, 70);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(248, 152);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(6, 109);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(134, 36);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.Text = "Schnell";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(6, 25);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(156, 36);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "Langsam";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(6, 67);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(130, 36);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Noraml";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // StartButten
+            // 
+            this.StartButten.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButten.Location = new System.Drawing.Point(688, 228);
+            this.StartButten.Name = "StartButten";
+            this.StartButten.Size = new System.Drawing.Size(248, 100);
+            this.StartButten.TabIndex = 2;
+            this.StartButten.Text = "Start";
+            this.StartButten.UseVisualStyleBackColor = true;
+            this.StartButten.Click += new System.EventHandler(this.StartButten_Click);
+            // 
+            // Feld
+            // 
+            this.Feld.ColumnCount = 20;
+            this.Feld.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.Feld.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.Feld.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 21F));
+            this.Feld.Controls.Add(this.Kopfpanel, 13, 3);
+            this.Feld.Location = new System.Drawing.Point(206, 70);
+            this.Feld.Name = "Feld";
+            this.Feld.RowCount = 20;
+            this.Feld.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.Feld.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.Feld.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Feld.Size = new System.Drawing.Size(395, 405);
+            this.Feld.TabIndex = 3;
+            // 
+            // Kopfpanel
+            // 
+            this.Kopfpanel.BackColor = System.Drawing.SystemColors.ControlText;
+            this.Kopfpanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Kopfpanel.Location = new System.Drawing.Point(257, 67);
+            this.Kopfpanel.Name = "Kopfpanel";
+            this.Kopfpanel.Size = new System.Drawing.Size(14, 14);
+            this.Kopfpanel.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 600);
+            this.Controls.Add(this.Feld);
+            this.Controls.Add(this.StartButten);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.Feld.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button StartButten;
+        private System.Windows.Forms.TableLayoutPanel Feld;
+        private System.Windows.Forms.Panel Kopfpanel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake_Winforms_Spiel
+{
+    public partial class Form1 : Form
+    {
+        bool oben, unten, rechts, links;
+       
+        Panel ApfelPanel = new Panel();
+
+        Random zufall = new Random();
+        List<Panel> Schlange = new List<Panel>();
+
+        List<TableLayoutPanelCellPosition> Zellen = new List<TableLayoutPanelCellPosition>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Geschwindigkeit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+
+                    if (!rechts)
+                    
+
+
+                        unten = oben = rechts = false;
+                        links = true;
+                        break;
+                    
+              case Keys.Right:
+
+                    if (!links)
+
+                    unten = oben = links = false;
+                    rechts = true;
+                    break;
+
+              case Keys.Up:
+
+                    if (!unten)
+
+                    unten = rechts = links = false;
+                    oben = true;
+                    break;
+
+              case Keys.Down:
+
+                    if (!oben)
+
+                    oben = links = rechts = false;
+                    unten = true;
+                    break;
+
+                deflaut:
+                    break;
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StartButten_Click(object sender, EventArgs e)
+        {
+            ApfelPanel.BackColor = SystemColors.Highlight;
+            ApfelPanel.Size=new Size(13,13);
+
+          Feld.Controls.Add(ApfelPanel,zufall.Next(0,Feld.ColumnCount),zufall.Next(0,Feld.RowCount));
+
+          
+
+            if (radioButton1.Checked)
+            {
+                timer1.Interval = 700;
+
+            }
+            else if (radioButton2.Checked) 
+            {
+                timer1.Interval = 300;
+
+            }
+            else if (radioButton3.Checked)
+            {
+                timer1.Interval = 100;
+
+            }
+
+            groupBox1.Enabled = false;
+            StartButten.Enabled = false;
+
+            timer1.Enabled = true;
+
+            Schlange.Add(Kopfpanel);
+            Zellen.Add(Feld.GetCellPosition(Kopfpanel));
+
+            unten = true;
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Zellen[0] = Feld.GetCellPosition(Kopfpanel);
+
+            if (unten) 
+            {
+                Feld.SetCellPosition(Kopfpanel, new TableLayoutPanelCellPosition(Feld.GetColumn(Kopfpanel),Feld.GetRow(Kopfpanel)+1));
+            }
+            if (oben)
+            {
+                Feld.SetCellPosition(Kopfpanel, new TableLayoutPanelCellPosition(Feld.GetColumn(Kopfpanel), Feld.GetRow(Kopfpanel) - 1));
+            }
+            if (rechts)
+            {
+                Feld.SetCellPosition(Kopfpanel, new TableLayoutPanelCellPosition(Feld.GetColumn(Kopfpanel) +1, Feld.GetRow(Kopfpanel)));
+            }
+            if (links)
+            {
+                Feld.SetCellPosition(Kopfpanel, new TableLayoutPanelCellPosition(Feld.GetColumn(Kopfpanel) -1, Feld.GetRow(Kopfpanel)));
+
+            }
+            for (int i = 1; i < Schlange.Count; i++) 
+            {
+                Zellen[i] = Feld.GetCellPosition(Schlange[i]);
+                Feld.SetCellPosition(Schlange[i], Zellen[i-1]);
+            }
+
+            if (Feld.GetCellPosition(Kopfpanel).Equals(Feld.GetCellPosition(ApfelPanel)))
+            {
+                Panel PlusPanal = new Panel();
+                PlusPanal.BackColor = SystemColors.ControlText;
+                PlusPanal.Size= new Size(13,13);
+
+                Schlange.Add(PlusPanal);
+                Zellen.Add(new TableLayoutPanelCellPosition(Zellen[Zellen.Count-1].Column, Zellen[Zellen.Count - 1].Row));
+
+                Feld.Controls.Add(PlusPanal, Zellen[Zellen.Count - 1].Column, Zellen[Zellen.Count - 1].Row);
+
+                Feld.SetCellPosition(ApfelPanel, new TableLayoutPanelCellPosition(zufall.Next(0, Feld.ColumnCount), zufall.Next(0, Feld.RowCount)));
+            }
+        }
+    }
+}
